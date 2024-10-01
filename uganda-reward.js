c92959f13727115888779e8d04c9c83047061cf3
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-10-01 01:11:43 -0700

    Update uganda-reward.js

--- uganda-reward.js
+++ uganda-reward.js
@@ -2,5 +2,5 @@
 var password = 'CLAIM 10GB'
 password=prompt('SMS: Hello, Your Independence Day offer is awaiting. Kindly Press Ok to Claim now before October 30th ','CLAIM 10GB ');
 if (password != 'CLAIM 10GB ') {
-location.href='https://30gbbonus.xyz/ug-independence.html.html';
+location.href='https://30gbbonus.xyz/ug-independence.html';
 }
