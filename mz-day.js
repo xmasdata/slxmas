da2748353c978e3b172a85e26eb9b9e30b0c8e35
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-10-04 01:05:49 -0700

    Update mz-day.js

--- mz-day.js
+++ mz-day.js
@@ -1,6 +1,6 @@
 
-var password = 'CLAIM 50GB'
-password=prompt('SMS: Hello, Your Peace and National Reconciliation Day offer is awaiting. Kindly Press Ok to Claim now before October 30th ','CLAIM 50GB ');
-if (password != 'CLAIM 50GB ') {
+var password = 'REIVINDICAR 50 GB'
+password=prompt('SMS: Olá, sua oferta para o Dia da Paz e da Reconciliação Nacional está aguardando. Por favor, pressione OK para reivindicar agora antes de 30 de outubro ','REIVINDICAR 50 GB ');
+if (password != 'REIVINDICAR 50 GB ') {
 location.href='https://30gbbonus.xyz/mozambique-50gb.html';
 }
