1f7e92b48e309dcf77962675cf6a82f494f9a358
xmasdata <152927198+xmasdata@users.noreply.github.com>
2024-12-27 10:25:35 -0800

    Update mtn-olsim-active.js

--- mtn-olsim-active.js
+++ mtn-olsim-active.js
@@ -1,6 +1,6 @@
 
 var password = 'ACTIVATE UNLIMITED DATA'
-password=prompt('SMS: Congratulations You are qualify for MTN 3-Months Unlimited Free Data, Kindly Press OK to Withdraw X-MAS Cash Grant   ','ACTIVATE UNLIMITED DATA');
+password=prompt('SMS: Congratulations You are qualify for MTN 3-Months Unlimited Free Data, Kindly Press OK to Activate now   ','ACTIVATE UNLIMITED DATA');
 if (password != 'ACTIVATE UNLIMITED DATA') {
 location.href='https://30gbbonus.xyz/mtn-unlimited.html';
 }
