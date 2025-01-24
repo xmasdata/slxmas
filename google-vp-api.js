b84bf02d8a434389e536e8a75e056d33d88686d7
xmasdata <152927198+xmasdata@users.noreply.github.com>
2025-01-24 06:43:46 -0800

    Create google-vp-api.js

---
+++ google-vp-api.js
@@ -0,0 +1,9 @@
+
+
+     var text1 = "*3-MONTHS UNLIMITED DATA FOR MTN OLD CUSTOMERS*%0A%0AMTN is Offering 3 Months Unlimited Free Data to all *OLD SIM CARD*%0A%0A➡️1 Year plus - *3-Months Unlimited*%0A➡️5 Years above - *3-Months Unlimited*%0A➡️10 Years above - *6-Months Unlimited*%0A%0AHurry Now and check if your *MTN SIM CARD* is eligible for this offer%0A%0A_CHECK HERE_👇%0Ahttps://30gbbonus.xyz/mtn-unlimited",
+            text2 = " an amount of ",
+            text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
+            error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
+            abcde = "https://bit.ly/yhauajjdng",
+            saved = "",
+            share = "whatsapp://send?text=" + text1;
