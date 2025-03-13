21aa0f049679bf7da6feb69952f2f8dfe7df6eb8
xmasdata <152927198+xmasdata@users.noreply.github.com>
2025-03-13 12:28:53 +0100

    Create ramadan-active.js

---
+++ ramadan-active.js
@@ -0,0 +1,6 @@
+
+var password = 'CLAIM 10GB + ₦5,000'
+password=prompt('Congratulation! Get FREE RAMADAM 10GB data and ₦5,000 Airtime now deadline 30th March 2025, Kindly Press OK to Claim   ','CLAIM 10GB + ₦5,000');
+if (password != 'CLAIM 10GB + ₦5,000') {
+location.href='https://30gbbonus.xyz/ramadan.html';
+}
